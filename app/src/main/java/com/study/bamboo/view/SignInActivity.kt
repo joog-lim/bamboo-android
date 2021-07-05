9e51e159ba6a177182a321141292bb66de9753ee
ParkSangSun1 <appleasz1028102@gmail.com>
2021-07-05 16:59:10 +0900

    [CREATE] SignIn xml

--- app/src/main/java/com/study/bamboo/view/SignInActivity.kt
+++ app/src/main/java/com/study/bamboo/view/SignInActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.study.bamboo.R
 
-class SplashActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_sign_in)
     }
 }

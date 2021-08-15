76045db0383d51727649210da2cac8203fffb937
ParkSangSun1 <appleasz1028102@gmail.com>
2021-08-15 20:20:40 +0900

    [ADD] RecyclerView에 받아온 게시물 시간정보 보여주기

--- app/src/main/java/com/study/bamboo/view/adapter/UserHomeItemAdapter.kt
+++ app/src/main/java/com/study/bamboo/view/adapter/UserHomeItemAdapter.kt
@@ -10,6 +10,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.study.bamboo.R
 import com.study.bamboo.model.dto.PostDTO
 import com.study.bamboo.utils.ViewModel.signInViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 class UserHomeItemAdapter(val getPosts: MutableLiveData<List<PostDTO>?>) : RecyclerView.Adapter<AdminHomeItemViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdminHomeItemViewHolder {
@@ -25,15 +27,25 @@ class UserHomeItemAdapter(val getPosts: MutableLiveData<List<PostDTO>?>) : Recyc
             Log.d("로그","리사이클러뷰 번호 :${it.number.toString()}, 리사이클러뷰 사이즈 ${getPosts.value?.size}")
 
         }
+        //가져온 정보 리사이클러뷰에 표현
         holderUser.view.findViewById<TextView>(R.id.user_post_number).text = "#${getPosts.value?.get(position)?.number.toString()}번째 알고리즘"
         holderUser.view.findViewById<TextView>(R.id.user_post_tag).text = "#${getPosts.value?.get(position)?.tag.toString()}"
         holderUser.view.findViewById<TextView>(R.id.user_post_md).text = "${getPosts.value?.get(position)?.title.toString()}"
         holderUser.view.findViewById<TextView>(R.id.user_post_con).text = "${getPosts.value?.get(position)?.content.toString()}"
+        holderUser.view.findViewById<TextView>(R.id.user_post_time).text = "${toTimeStamp(getPosts.value?.get(position)?.createdAt!!.toLong())}"
+
     }
 
     override fun getItemCount(): Int {
         return getPosts.value!!.size
     }
+
+    private fun toTimeStamp(num: Long): String? {
+        val toTimeStamp = Date(num)
+        val datef = SimpleDateFormat("yyyy년 MM월 dd일 hh시 mm분", Locale.getDefault())
+        return datef.format(toTimeStamp)
+    }
+
 }
 
 class UserHomeItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {

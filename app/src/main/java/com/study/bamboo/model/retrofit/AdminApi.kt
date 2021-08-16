53b40958ad7beaa626b9d8b1a74aeefa66f50044
KyungMin <2004ckck@naver.com>
2021-08-16 18:16:25 +0900

    [CREATE] admin Api 생성

---
+++ app/src/main/java/com/study/bamboo/model/retrofit/AdminApi.kt
@@ -0,0 +1,25 @@
+package com.study.bamboo.model.retrofit
+
+import com.study.bamboo.model.dto.DeletePostDto
+import com.study.bamboo.model.dto.GetPostDTO
+import com.study.bamboo.model.dto.PostDTO
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface AdminApi {
+    @DELETE("delete/{arg}")
+   suspend fun deletePost(
+        @Path("arg") arg: String
+    ): Response<DeletePostDto>
+
+    @GET("post/get-list")
+  suspend  fun getPost(
+        @Query("count") count : Int,
+        @Query("cursor") cursor : String,
+        @Query("status") status : String
+    ) : Response<GetPostDTO>
+}

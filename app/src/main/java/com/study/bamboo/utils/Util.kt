be8efcd549111c10a31241f48ff1b7832033e7e0
KyungMin <2004ckck@naver.com>
2021-08-25 13:15:06 +0900

    [CREATE] dataStore 변수 생성

---
+++ app/src/main/java/com/study/bamboo/utils/Util.kt
@@ -0,0 +1,12 @@
+package com.study.bamboo.utils
+
+class Util {
+
+
+    companion object{
+
+        const val PREFERENCE_NAME="token_preference"
+        const val DEFAULT_TOKEN="default token"
+        const val PREFERENCES_TOKEN = "preferences_token"
+    }
+}

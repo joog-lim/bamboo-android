543e2fd360bd84d909908d8b7aef61fb3fefdec1
KyungMin <2004ckck@naver.com>
2021-08-25 18:25:22 +0900

    [ADD] status 없는 함수 추가

--- app/src/main/java/com/study/bamboo/view/fragment/admin/AdminRepository.kt
+++ app/src/main/java/com/study/bamboo/view/fragment/admin/AdminRepository.kt
@@ -1,6 +1,5 @@
 package com.study.bamboo.view.fragment.admin
 
-import com.study.bamboo.adapter.Status
 import com.study.bamboo.model.retrofit.AdminApi
 import javax.inject.Inject
 
@@ -10,8 +9,11 @@ class AdminRepository @Inject constructor(
 ) {
     suspend fun deletePost(token:String,message: String, arg: String) = adminApi.deletePost(token,message, arg)
 
-    suspend fun patchPost(token:String,id: String, status:Status,title: String, content: String, reason: String) =
+    suspend fun patchPost(token:String,id: String, status:String,title: String, content: String, reason: String) =
         adminApi.patchPost(token,id, status,title, content, reason)
+    suspend fun acceptPatchPost(token:String,id: String,title: String, content: String, reason: String) =
+        adminApi.acceptPatchPost(token,id,title, content, reason)
+
 
     suspend fun getPost(token: String, count: Int, cursor: String, status: String) =
         adminApi.getPost(token, count, cursor, status)

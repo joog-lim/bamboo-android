f47b7c45503bba701537720e6b9a259a69964a1f
KyungMin <2004ckck@naver.com>
2021-08-25 13:14:28 +0900

    [DELETE] SignInViewModel 제거

--- app/src/main/java/com/study/bamboo/view/activity/splash/SplashActivity.kt
+++ app/src/main/java/com/study/bamboo/view/activity/splash/SplashActivity.kt
@@ -7,11 +7,9 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.study.bamboo.R
 import com.study.bamboo.utils.ViewModel.postCreateViewModel
-import com.study.bamboo.utils.ViewModel.signInViewModel
 import com.study.bamboo.utils.ViewModel.splashViewModel
 import com.study.bamboo.view.activity.signin.SignInActivity
 import com.study.bamboo.view.activity.postcreate.PostCreateViewModel
-import com.study.bamboo.view.activity.signin.SignInViewModel
 
 class SplashActivity : AppCompatActivity() {
 
@@ -25,10 +23,6 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun initViewModel(){
-        signInViewModel = ViewModelProvider(
-            this,
-            ViewModelProvider.NewInstanceFactory()
-        ).get(SignInViewModel::class.java)
 
         postCreateViewModel = ViewModelProvider(
             this,

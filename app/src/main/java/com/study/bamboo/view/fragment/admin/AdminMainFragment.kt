d1d2794e510f3235588bdeaf21ff52b2f0e1c1b5
KyungMin <2004ckck@naver.com>
2021-08-27 14:29:24 +0900

    [ADD] 리사이클러뷰 adapter 추가

--- app/src/main/java/com/study/bamboo/view/fragment/admin/AdminMainFragment.kt
+++ app/src/main/java/com/study/bamboo/view/fragment/admin/AdminMainFragment.kt
@@ -9,16 +9,23 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.study.bamboo.LinearLayoutManagerWrapper
 import com.study.bamboo.R
 import com.study.bamboo.adapter.AdminHomeItemAdapter
 import com.study.bamboo.adapter.AdminHomeItemAdapter.Companion.ACCEPTEDType
 import com.study.bamboo.adapter.AdminHomeItemAdapter.Companion.DELETEDType
 import com.study.bamboo.adapter.AdminHomeItemAdapter.Companion.PENDINGType
+import com.study.bamboo.adapter.PostLoadingAdapter
 import com.study.bamboo.databinding.FragmentAdminMainBinding
 import com.study.bamboo.view.base.BaseFragment
+import com.study.bamboo.view.fragment.admin.paging.PagingPostViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
+// TODO: 2021-08-26
 
 @AndroidEntryPoint
 class AdminMainFragment : BaseFragment<FragmentAdminMainBinding>(R.layout.fragment_admin_main) {
@@ -28,6 +35,7 @@ class AdminMainFragment : BaseFragment<FragmentAdminMainBinding>(R.layout.fragme
     }
 
     private lateinit var viewModel: AdminViewModel
+    private lateinit var pagingViewModel: PagingPostViewModel
 
     private val acceptAdapter: AdminHomeItemAdapter by lazy {
         AdminHomeItemAdapter(ACCEPTEDType)
@@ -45,10 +53,11 @@ class AdminMainFragment : BaseFragment<FragmentAdminMainBinding>(R.layout.fragme
     private val pendingAdapter: AdminHomeItemAdapter by lazy {
         AdminHomeItemAdapter(PENDINGType)
     }
-
+    private var job: Job? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(requireActivity()).get(AdminViewModel::class.java)
+        pagingViewModel = ViewModelProvider(requireActivity()).get(PagingPostViewModel::class.java)
     }
 
     var token = ""
@@ -102,7 +111,6 @@ class AdminMainFragment : BaseFragment<FragmentAdminMainBinding>(R.layout.fragme
                             lifecycleScope.launch {
                                 observeNetwork(
                                     token,
-                                    20,
                                     "",
                                     "ACCEPTED"
                                 )
@@ -116,7 +124,7 @@ class AdminMainFragment : BaseFragment<FragmentAdminMainBinding>(R.layout.fragme
                             lifecycleScope.launch {
                                 observeNetwork(
                                     token,
-                                    20,
+
                                     "",
                                     "PENDING"
                                 )
@@ -130,7 +138,7 @@ class AdminMainFragment : BaseFragment<FragmentAdminMainBinding>(R.layout.fragme
                             lifecycleScope.launch {
                                 observeNetwork(
                                     token,
-                                    20,
+
                                     "",
                                     "REJECTED"
                                 )
@@ -145,7 +153,7 @@ class AdminMainFragment : BaseFragment<FragmentAdminMainBinding>(R.layout.fragme
                             lifecycleScope.launch {
                                 observeNetwork(
                                     token,
-                                    20,
+
                                     "",
                                     "DELETED"
                                 )
@@ -160,7 +168,6 @@ class AdminMainFragment : BaseFragment<FragmentAdminMainBinding>(R.layout.fragme
                             lifecycleScope.launch {
                                 observeNetwork(
                                     token,
-                                    20,
                                     "",
                                     "ACCEPTED"
                                 )
@@ -173,32 +180,92 @@ class AdminMainFragment : BaseFragment<FragmentAdminMainBinding>(R.layout.fragme
     }
 
 
-    suspend fun observeNetwork(token: String, count: Int, cursor: String, status: String) {
-        Log.d(TAG, "observeNetwork: $status")
+    private fun recyclerViewEnd(recyclerView: RecyclerView, adapter: AdminHomeItemAdapter) {
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+
+                val lastVisibleItemPosition =
+                    (recyclerView.layoutManager as LinearLayoutManager?)!!.findLastCompletelyVisibleItemPosition() // 화면에 보이는 마지막 아이템의 position
+                val itemTotalCount = recyclerView.adapter!!.itemCount - 1 // 어댑터에 등록된 아이템의 총 개수 -1
+
+                Log.d(TAG, "현재 포지션: $lastVisibleItemPosition")
+
+                // 스크롤이 끝에 도달했는지 확인
+                if (lastVisibleItemPosition == itemTotalCount) {
+                    Log.d(TAG, "onScrolled: 마지막 포지션: $lastVisibleItemPosition")
+                } else {
+
+                }
+            }
+        })
+    }
+
+
+    // Post observe (Paging x)
+    private fun observeData(adapter: AdminHomeItemAdapter) {
+        viewModel.getPostData.observe(viewLifecycleOwner, {
+            adapter.setItemList(it)
+        })
+    }
+
+    // paging에 먼저 피마리터 값 보내줌
+    private fun observeGetData(token: String, cursor: String, status: String) {
+        pagingViewModel.getData(token, cursor, status)
+    }
+
+    // paging 데이터값 받아옴
+    private fun observePagingData(adapter: AdminHomeItemAdapter) {
+
+
+        job = lifecycleScope.launch {
+
+            pagingViewModel.flow.collectLatest {
+                adapter.submitData( viewLifecycleOwner.lifecycle, it)
+                Log.d(TAG, "getPost: $it")
+            }
+
+//            pagingViewModel.photos.observe(viewLifecycleOwner) {
+//                adapter.submitData(viewLifecycleOwner.lifecycle, it)
+//                Log.d(TAG, "getPost: $it")
+//            }
+        }
+
+    }
+
+
+    suspend fun observeNetwork(token: String, cursor: String, status: String) {
         when (status) {
-            "ACCEPTED" -> lifecycleScope.launch {
-                viewModel.getPostData(token, count, cursor, status)
-                // status에 따라 post 가져옴
-                observeGetPost(acceptAdapter)
+            "ACCEPTED" -> {
+                // 데이터값을 보냄 (Paging x)
+                observeGetData(token, cursor, status)
+
+
+//                //먼저 데이터값을 adapter에 넣음
+//                observeData(acceptAdapter)
+//                viewModel.getPost(token, 20, cursor, status)
+
+
+
+                observePagingData(acceptAdapter)
+                recyclerViewEnd(binding.postRecyclerView,acceptAdapter)
+                // 잘 넣어지면 게시물의 position 이 마지막으로 되면 페이징 으로 데이터를 보냄
+//                recyclerViewEnd(binding.postRecyclerView, acceptAdapter)
 
             }
-            "PENDING" -> lifecycleScope.launch {
-                Log.d(TAG, "observeNetwork: $token")
-                viewModel.getPostData(token, count, cursor, status)
-                observeGetPost(pendingAdapter)
-
-
+            "PENDING" -> {
+                // 데이터값을 보냄 (Paging x)
+                observeGetData(token, cursor, status)
+                observePagingData(pendingAdapter)
             }
-            "REJECTED" -> lifecycleScope.launch {
-
-                viewModel.getPostData(token, count, cursor, status)
-                observeGetPost(rejectAdapter)
-
+            "REJECTED" -> {
+                observeGetData(token, cursor, status)
+                observePagingData(rejectAdapter)
             }
-            "DELETED" -> lifecycleScope.launch {
-                viewModel.getPostData(token, count, cursor, status)
-                observeGetPost(deleteAdapter)
-
+            "DELETED" -> {
+                observeGetData(token, cursor, status)
+                observePagingData(deleteAdapter)
             }
 
         }
@@ -206,20 +273,20 @@ class AdminMainFragment : BaseFragment<FragmentAdminMainBinding>(R.layout.fragme
     }
 
 
-    private fun observeGetPost(adapter: AdminHomeItemAdapter) {
-        viewModel.getPostData.observe(viewLifecycleOwner, {
-            Log.d(TAG, "observeGetPost: $it")
-            adapter.setItemList(it)
-
-
-        })
-    }
-
-
     private fun setItemAdapter(adapter: AdminHomeItemAdapter) {
+
+
+        val linearLayoutManagerWrapepr =
+            context?.let { LinearLayoutManagerWrapper(it, LinearLayoutManager.VERTICAL, false) }
+
+
         binding.postRecyclerView.apply {
             this.adapter = adapter
-            this.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            this.adapter = adapter.withLoadStateHeaderAndFooter(
+                header = PostLoadingAdapter { adapter.retry() },
+                footer = PostLoadingAdapter { adapter.retry() }
+            )
+            this.layoutManager = linearLayoutManagerWrapepr
         }
     }
 }

a45c45978424f5627f264c710908b62098885b07
KyungMin <2004ckck@naver.com>
2021-08-28 20:32:45 +0900

    [CREATE] Admin sealed class 생성

---
+++ app/src/main/java/com/study/bamboo/utils/Admin.kt
@@ -0,0 +1,46 @@
+package com.study.bamboo.utils
+
+import com.study.bamboo.model.dto.UserPostDTOBase
+
+sealed class Admin {
+
+    data class Accept(
+        override val content: String,
+        override val createdAt: Long,
+        override val id: String,
+        override val number: Int,
+        override val status: String,
+        override val tag: String,
+        override val title: String
+    ) : UserPostDTOBase
+
+    data class Delete(
+        override val content: String,
+        override val createdAt: Long,
+        override val id: String,
+        override val number: Int,
+        override val status: String,
+        override val tag: String,
+        override val title: String
+    ) : UserPostDTOBase
+
+    data class Reject(
+        override val content: String,
+        override val createdAt: Long,
+        override val id: String,
+        override val number: Int,
+        override val status: String,
+        override val tag: String,
+        override val title: String
+    ) : UserPostDTOBase
+
+    data class Pending(
+        override val content: String,
+        override val createdAt: Long,
+        override val id: String,
+        override val number: Int,
+        override val status: String,
+        override val tag: String,
+        override val title: String
+    ) : UserPostDTOBase
+}

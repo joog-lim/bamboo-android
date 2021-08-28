5f48097da5a2549be0c3882a5085b5edc5a46514
KyungMin <2004ckck@naver.com>
2021-08-28 23:38:51 +0900

    [UPDATE] 피라미터 위치 변경

--- app/src/main/java/com/study/bamboo/view/fragment/admin/AdminRepository.kt
+++ app/src/main/java/com/study/bamboo/view/fragment/admin/AdminRepository.kt
@@ -7,13 +7,20 @@ import javax.inject.Inject
 class AdminRepository @Inject constructor(
     private val adminApi: AdminApi
 ) {
-    suspend fun deletePost(token:String,message: String, arg: String) = adminApi.deletePost(token,message, arg)
+    suspend fun deletePost(token: String, reason: String, id: String) =
+        adminApi.deletePost(token, id, reason)
 
-    suspend fun patchPost(token:String,id: String, status:String,title: String, content: String, reason: String) =
-        adminApi.patchPost(token,id, status,title, content, reason)
-    suspend fun acceptPatchPost(token:String,id: String,title: String, content: String, reason: String) =
-        adminApi.acceptPatchPost(token,id,title, content, reason)
+    suspend fun patchPost(token: String, id: String, status: String) =
+        adminApi.patchPost(token, id, status)
 
+    suspend fun acceptPatchPost(
+        token: String,
+        id: String,
+        title: String,
+        content: String,
+        reason: String
+    ) =
+        adminApi.acceptPatchPost(token, id, title, content, reason)
 
 
 }

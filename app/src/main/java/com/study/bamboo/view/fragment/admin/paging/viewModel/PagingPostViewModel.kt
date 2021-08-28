00e9c2f259433e19ef65c44392bc6d14be971d11
KyungMin <2004ckck@naver.com>
2021-08-28 23:43:34 +0900

    [CREATE] Paging 관련 viewModel 생성

---
+++ app/src/main/java/com/study/bamboo/view/fragment/admin/paging/viewModel/PagingPostViewModel.kt
@@ -0,0 +1,100 @@
+package com.study.bamboo.view.fragment.admin.paging.viewModel
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.cachedIn
+import com.study.bamboo.model.retrofit.AdminApi
+import com.study.bamboo.view.fragment.admin.paging.AcceptPagingSource
+import com.study.bamboo.view.fragment.admin.paging.AcceptPagingSource.Companion.UNSPLASH_STARTING_PAGE_INDEX
+import com.study.bamboo.view.fragment.admin.paging.DeletePagingSource
+import com.study.bamboo.view.fragment.admin.paging.PendingPagingSource
+import com.study.bamboo.view.fragment.admin.paging.RejectPagingSource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class PagingPostViewModel @Inject constructor(
+    application: Application,
+    private val adminApi: AdminApi
+) : AndroidViewModel(application) {
+    private val TAG = "PagingPostViewModel"
+
+    val token = MutableLiveData<String>()
+    val cursor = MutableLiveData<String>()
+
+
+    fun getData(token: String, cursor: String, status: String) {
+        this.token.value = token
+        this.cursor.value = cursor
+    }
+
+
+    val acceptData = Pager(
+        // Configure how data is loaded by passing additional properties to
+        // PagingConfig, such as prefetchDistance.
+        PagingConfig(
+            pageSize = UNSPLASH_STARTING_PAGE_INDEX,
+            enablePlaceholders = false
+        )
+    ) {
+        AcceptPagingSource(
+            adminApi,
+            token.value.toString(),
+            cursor.value.toString(),
+        )
+    }.flow
+        .cachedIn(viewModelScope)
+
+    val pendingData = Pager(
+
+        PagingConfig(
+            pageSize = UNSPLASH_STARTING_PAGE_INDEX,
+            enablePlaceholders = false
+        )
+    ) {
+        PendingPagingSource(
+            adminApi,
+            token.value.toString(),
+            cursor.value.toString(),
+        )
+    }.flow
+        .cachedIn(viewModelScope)
+
+    val rejectData = Pager(
+
+        PagingConfig(
+            pageSize = UNSPLASH_STARTING_PAGE_INDEX,
+            enablePlaceholders = false
+        )
+    ) {
+        RejectPagingSource(
+            adminApi,
+            token.value.toString(),
+            cursor.value.toString(),
+        )
+    }.flow
+        .cachedIn(viewModelScope)
+
+    val deleteData = Pager(
+
+        PagingConfig(
+            pageSize = UNSPLASH_STARTING_PAGE_INDEX,
+            enablePlaceholders = false
+        )
+    ) {
+        DeletePagingSource(
+            adminApi,
+            token.value.toString(),
+            cursor.value.toString(),
+        )
+    }.flow
+        .cachedIn(viewModelScope)
+
+
+}

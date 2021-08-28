164b6a6b73ed3f9b4a75a56c99edfb89355ba462
KyungMin <2004ckck@naver.com>
2021-08-28 23:42:02 +0900

    [CREATE] AdminDeleteAdapter PagingAdapter 생성

---
+++ app/src/main/java/com/study/bamboo/adapter/admin/AdminDeleteAdapter.kt
@@ -0,0 +1,82 @@
+package com.study.bamboo.adapter.admin
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.study.bamboo.R
+import com.study.bamboo.databinding.AdminPostDeleteRecyclerItemBinding
+import com.study.bamboo.utils.Admin
+import com.study.bamboo.view.fragment.admin.AdminMainFragmentDirections
+
+class AdminDeleteAdapter :
+    PagingDataAdapter<Admin.Delete, AdminDeleteAdapter.AdminDeleteItemViewHolder>(diffCallback) {
+
+    companion object {
+
+        private val diffCallback = object : DiffUtil.ItemCallback<Admin.Delete>() {
+            override fun areItemsTheSame(
+                oldItem: Admin.Delete,
+                newItem: Admin.Delete
+            ): Boolean =
+                oldItem.id == newItem.id
+
+
+            override fun areContentsTheSame(
+                oldItem: Admin.Delete,
+                newItem: Admin.Delete
+            ): Boolean =
+                oldItem.content == newItem.content
+
+
+        }
+    }
+
+    override fun onBindViewHolder(holder: AdminDeleteItemViewHolder, position: Int) {
+        val item = getItem(position)
+        if (item != null) {
+
+            holder.bind(item)
+            holder.binding.postMore.setOnClickListener {
+                val action =
+                    AdminMainFragmentDirections.actionAdminMainFragmentToDeleteDialog(
+                        item.id
+                    )
+                it.findNavController().navigateUp()
+                it.findNavController().navigate(action)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdminDeleteItemViewHolder {
+        return AdminDeleteItemViewHolder.from(parent)
+    }
+
+    //삭제
+    class AdminDeleteItemViewHolder(val binding: AdminPostDeleteRecyclerItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+
+        fun bind(data: Admin.Delete) {
+            binding.data = data
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): AdminDeleteItemViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding: AdminPostDeleteRecyclerItemBinding = DataBindingUtil
+                    .inflate(
+                        layoutInflater, R.layout.admin_post_delete_recycler_item,
+                        parent, false
+                    )
+                return AdminDeleteItemViewHolder(binding)
+            }
+        }
+    }
+}
+

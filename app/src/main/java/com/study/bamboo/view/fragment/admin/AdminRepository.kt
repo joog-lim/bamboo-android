09ad7b2d20189a699f2203e75903de41682b13ee
KyungMin <2004ckck@naver.com>
2021-08-29 15:35:40 +0900

    [UPDATE] String-> HashMap<String,String> 으로 변경

--- app/src/main/java/com/study/bamboo/view/fragment/admin/AdminRepository.kt
+++ app/src/main/java/com/study/bamboo/view/fragment/admin/AdminRepository.kt
@@ -7,10 +7,10 @@ import javax.inject.Inject
 class AdminRepository @Inject constructor(
     private val adminApi: AdminApi
 ) {
-    suspend fun deletePost(token: String, reason: String, id: String) =
-        adminApi.deletePost(token, id, reason)
+    suspend fun deletePost( token:String,reason: String, id: String) =
+        adminApi.deletePost(token ,arg = id, reason)
 
-    suspend fun patchPost(token: String, id: String, status: String) =
+    suspend fun patchPost(token: String, id: String, status:  HashMap<String, String>) =
         adminApi.patchPost(token, id, status)
 
     suspend fun acceptPatchPost(
@@ -18,9 +18,9 @@ class AdminRepository @Inject constructor(
         id: String,
         title: String,
         content: String,
-        reason: String
+        tag: String
     ) =
-        adminApi.acceptPatchPost(token, id, title, content, reason)
+        adminApi.acceptPatchPost(token, id, title, content, tag)
 
 
 }

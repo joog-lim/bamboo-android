91961cd479a57c3fd7052f5bb292bffd7065d639
KyungMin <2004ckck@naver.com>
2021-08-29 13:56:08 +0900

    [UPDATE] BASE_URL 변경

--- app/src/main/java/com/study/bamboo/utils/Retrofit.kt
+++ app/src/main/java/com/study/bamboo/utils/Retrofit.kt
@@ -1,5 +1,5 @@
 package com.study.bamboo.utils
 
 object Retrofit {
-    const val BASE_URL = "https://xdjffzeil5.execute-api.ap-northeast-2.amazonaws.com/api/"
+    const val BASE_URL = " https://ket73grkcf.execute-api.ap-northeast-2.amazonaws.com/apiV2/"
 }

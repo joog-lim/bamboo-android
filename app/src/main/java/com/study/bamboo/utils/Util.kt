a56c78b9304536900be1cf0441722765e1a8a9b6
KyungMin <2004ckck@naver.com>
2021-08-30 16:37:11 +0900

    [ADD] baseUrl util class로 추가

--- app/src/main/java/com/study/bamboo/utils/Util.kt
+++ app/src/main/java/com/study/bamboo/utils/Util.kt
@@ -8,5 +8,8 @@ class Util {
         const val PREFERENCE_NAME="token_preference"
         const val DEFAULT_TOKEN="default token"
         const val PREFERENCES_TOKEN = "preferences_token"
+
+        // base_url
+        const val BASE_URL = "https://ket73grkcf.execute-api.ap-northeast-2.amazonaws.com/apiV2/"
     }
 }

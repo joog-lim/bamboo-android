67e888cece3582078a5a34cc3e8ec9aec045b333
KyungMin <2004ckck@naver.com>
2021-08-30 20:46:35 +0900

    [ADD] dataBase name

--- app/src/main/java/com/study/bamboo/utils/Util.kt
+++ app/src/main/java/com/study/bamboo/utils/Util.kt
@@ -11,5 +11,15 @@ class Util {
 
         // base_url
         const val BASE_URL = "https://ket73grkcf.execute-api.ap-northeast-2.amazonaws.com/apiV2/"
+
+        // database name
+        const val DATABASE_NAME = "AdminDataBase"
+
+        // table name
+
+        const val Accept_Table="acceptTable"
+        const val Reject_Table="rejectTable"
+        const val Pending_Table="pendingTable"
+        const val Delete_Table="deleteTable"
     }
 }

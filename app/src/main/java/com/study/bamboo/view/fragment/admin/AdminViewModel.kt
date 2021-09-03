18730c65b714ebdde5809e0fd50fb62072a64503
KyungMin <2004ckck@naver.com>
2021-09-03 11:35:43 +0900

    [ADD] 데이터 성공여부에 따라 변수에 값 넣음

--- app/src/main/java/com/study/bamboo/view/fragment/admin/AdminViewModel.kt
+++ app/src/main/java/com/study/bamboo/view/fragment/admin/AdminViewModel.kt
@@ -5,9 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.study.bamboo.data.network.models.admin.AcceptModify
 import com.study.bamboo.data.repository.Repository
-import com.study.bamboo.utils.Admin
 import com.study.bamboo.utils.Util.Companion.DEFAULT_TOKEN
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
@@ -26,10 +24,17 @@ class AdminViewModel @Inject constructor(
     private val TAG = "AdminViewModel"
 
 
-
     private val _cursor = MutableLiveData<String>()
     val cursor: LiveData<String> get() = _cursor
 
+    private val _successPatchData = MutableLiveData<String?>()
+    val successPatchData: MutableLiveData<String?> get() = _successPatchData
+
+    private val _successAcceptData = MutableLiveData<String?>()
+    val successAcceptData: MutableLiveData<String?> get() = _successAcceptData
+
+    private val _successDeleteData = MutableLiveData<String?>()
+    val successDeleteData: MutableLiveData<String?> get() = _successDeleteData
 
 
     // 토큰을 저장한다.
@@ -48,42 +53,45 @@ class AdminViewModel @Inject constructor(
     ) = viewModelScope.launch {
         repository.remote.acceptPatchPost(token, id, bodyMap).let { response ->
 
+            var number = response.body()?.number
             if (response.isSuccessful) {
                 Log.d(TAG, "acceptPatchPost: 성공")
+                _successAcceptData.value = "${number}번째 게시물을 수정했습니다."
 
-            }else{
-                Log.d(TAG, "acceptPatchPost: ${response.message()}")
+            } else {
+                _successAcceptData.value = " ${response.message()}"
             }
         }
     }
 
     fun patchPost(token: String, id: String, status: HashMap<String, String>) =
         viewModelScope.launch {
-            repository.remote.patchPost(token, id, status).let {
+            repository.remote.patchPost(token, id, status).let { response ->
 
-                if (it.isSuccessful) {
+                val beforeStatus = response.body()?.beforeStatus
+                val afterStatus = response.body()?.afterStatus
+                if (response.isSuccessful) {
+                    _successPatchData.value = "$beforeStatus status 를 $afterStatus 로 바꾸었습니다."
 
                     Log.d(TAG, "patchPost: 성공!")
+                }else{
+                    _successPatchData.value=response.message()
                 }
 
-                Log.d(TAG, "patchPost: ${it.errorBody()}")
             }
         }
 
     fun deletePost(token: String, reason: String, id: String) = viewModelScope.launch {
 
         repository.remote.deletePost(token, reason, id).let { response ->
-
+            var number = response.body()?.number
             if (response.isSuccessful) {
+                _successDeleteData.value = "$number 의 게시물을 제거했습니다."
             } else {
                 Log.d(TAG, "deletePost: ${response.errorBody()}")
             }
         }
     }
-
-
-
-
 
 
 }

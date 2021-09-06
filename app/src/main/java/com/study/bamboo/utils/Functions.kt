0e08bd8a40fa585815309c26650d3c17fc296aed
ParkSangSun <appleasz1028102@gmail.com>
2021-09-06 12:04:21 +0900

    [ADD] Splash 화면에서 wifi 체크

--- app/src/main/java/com/study/bamboo/utils/Functions.kt
+++ app/src/main/java/com/study/bamboo/utils/Functions.kt
@@ -3,6 +3,14 @@ package com.study.bamboo.utils
 import android.app.Activity
 import android.content.Context
 import android.graphics.Point
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -20,11 +28,4 @@ object Functions {
         display.getSize(size)
         return size
     }
-
-/*    fun getStandardSize() {
-        val ScreenSize = getScreenSize(this)
-        density = getResources().getDisplayMetrics().density
-        standardSize_X = (ScreenSize!!.x / density) as Int
-        standardSize_Y = (ScreenSize.y / density) as Int
-    }*/
 }
--- app/src/main/java/com/study/bamboo/view/activity/splash/SplashActivity.kt
+++ app/src/main/java/com/study/bamboo/view/activity/splash/SplashActivity.kt
@@ -1,29 +1,67 @@
 package com.study.bamboo.view.activity.splash
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Point
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.study.bamboo.R
 import com.study.bamboo.base.BaseActivity
+import com.study.bamboo.utils.NetworkHandler
 import com.study.bamboo.utils.Variable
 import com.study.bamboo.view.activity.signin.SignInActivity
-import com.study.bamboo.view.activity.postcreate.PostCreateViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SplashActivity : BaseActivity() {
 
+    private lateinit var networkHandler: NetworkHandler
     private val splashViewModel by viewModels<SplashViewModel>()
+    private lateinit var wifiDialog : WifiDialog
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         supportActionBar!!.hide()
-        observeViewModel()
-        callViewModelApi()
+
+        networkHandler = NetworkHandler(this)
+        when (networkHandler.isNetworkAvailable()) {
+            true -> {
+                observeViewModel()
+                callViewModelApi()
+            }
+            false -> noUseWifi()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+
+    private fun noUseWifi() {
+        //다이얼로그
+        wifiDialog = WifiDialog()
+        wifiDialog.show(supportFragmentManager, "noUseWifiDialog")
+        val windowManager = this.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val display = windowManager.defaultDisplay
+        val size = Point()
+        display.getSize(size)
+        SignInActivity.deviceSizeX = size.x
     }
 
 

9e4bab6f0b2bbb298fb41f5d3d279c3c86156753
KyungMin <2004ckck@naver.com>
2021-09-26 14:04:53 +0900

    add 다크모드 비활성화

--- app/src/main/java/com/study/bamboo/utils/App.kt
+++ app/src/main/java/com/study/bamboo/utils/App.kt
@@ -1,6 +1,7 @@
 package com.study.bamboo.utils
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -13,5 +14,7 @@ class App : Application() {
     override fun onCreate(){
         super.onCreate()
         instance =this
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
     }
 }

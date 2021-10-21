c5fc5aa464f1663e7067fdea092e0ef81ec47c43
KyungMin <2004ckck@naver.com>
2021-10-21 11:39:56 +0900

    init

--- app/src/main/java/com/study/bamboo/utils/SampleViewEvents.kt
+++
@@ -1,8 +0,0 @@
-package com.study.bamboo.utils
-
-//sealed class SampleViewEvents {
-//    data class Edit(val sampleEntity: Admin.Accept) : SampleViewEvents()
-//    data class Remove(val sampleEntity: Admin.Accept) : SampleViewEvents()
-//    object InsertItemHeader : SampleViewEvents()
-//    object InsertItemFooter : SampleViewEvents()
-//}

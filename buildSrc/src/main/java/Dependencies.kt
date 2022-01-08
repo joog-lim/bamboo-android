749e4dcae5bdd144af08900c3bd2bab51832a9f1
ckrudals <2004ckck@naver.com>
2022-01-08 12:16:55 +0900

    add paging kts

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,18 +1,19 @@
 object Versions {
     const val KOTLIN_VERSION = "1.6.10"
-    const val KOTLINX_COROUTINES = "1.6.10"
+    const val CORE = "1.3.2"
+    const val KOTLINX_COROUTINES = "1.5.2"
     const val BUILD_GRADLE = "4.2.1"
-    const val COMPILE_SDK_VERSION = 31
+    const val COMPILE_SDK_VERSION = 30
     const val BUILD_TOOLS_VERSION = "30.0.3"
     const val MIN_SDK_VERSION = 23
-    const val TARGET_SDK_VERSION = 30
+    const val TARGET_SDK_VERSION = 330
     const val CORE_KTX = "1.5.0"
     const val APP_COMPAT = "1.3.0"
     const val ACTIVITY_KTX = "1.2.3"
     const val FRAGMENT_KTX = "1.3.4"
     const val LIFECYCLE_KTX = "2.3.1"
     const val ROOM = "2.3.0"
-    const val HILT = "2.35.1"
+    const val HILT = "2.38"
     const val MATERIAL = "1.3.0"
     const val RETROFIT = "2.7.1"
     const val JUNIT = "4.13.2"
@@ -23,9 +24,8 @@ object Versions {
     const val LEGACY_SUPPORT = "1.0.0"
     const val NAVIGATION = "2.3.5"
     const val GOOGLE_AUTH = "19.2.0"
-    const val DATASTORE = "1.0.0"
-    const val RX_BINDING = "4.0.0"
-    const val RX_JAVA = "3.0.6"
+    const val DATASTORE ="1.0.0"
+    const val RX_BINDING = "3.0.0"
     const val RX = "3.0.0"
     const val PAGING = "3.1.0"
 
@@ -38,7 +38,7 @@ object Kotlin {
     const val COROUTINES_CORE =
         "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.KOTLINX_COROUTINES}"
     const val CORE_KTX =
-        "androidx.core:core-ktx:${Versions.KOTLIN_VERSION}"
+        "androidx.core:core-ktx:${Versions.CORE}"
     const val COROUTINES_ANDROID =
         "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.KOTLINX_COROUTINES}"
 }
@@ -53,7 +53,7 @@ object AndroidX {
     const val NAVIGATION_FRAGMENT =
         "androidx.navigation:navigation-fragment-ktx:${Versions.NAVIGATION}"
     const val NAVIGATION_SUPPORT =
-        "androidx.navigation:navigation-ui-ktx${Versions.NAVIGATION}"
+        "androidx.navigation:navigation-ui-ktx:${Versions.NAVIGATION}"
     const val ACTIVITY_KTX = "androidx.activity:activity-ktx:${Versions.ACTIVITY_KTX}"
     const val FRAGMENT_KTX = "androidx.fragment:fragment-ktx:${Versions.FRAGMENT_KTX}"
     const val LIFECYCLE_VIEWMODEL_KTX =
@@ -64,7 +64,9 @@ object AndroidX {
     const val ROOM_KTX = "androidx.room:room-ktx:${Versions.ROOM}"
     const val ROOM_COMPILER =
         "androidx.room:room-compiler:${Versions.ROOM}"
-    const val DATASTORE = "androidx.datastore:datastore-preferences:${Versions.DATASTORE}"
+
+    const val DATASTORE_PREFERENCES="androidx.datastore:datastore-preferences:${Versions.DATASTORE}"
+    const val DATASTORE_CORE="androidx.datastore:datastore-core:${Versions.DATASTORE}"
 }
 
 object Google {
@@ -76,9 +78,9 @@ object Google {
 
 object RX {
     const val RX_ANDROID = "io.reactivex.rxjava3:rxandroid:${Versions.RX}"
-    const val RX_KOTLIN = "io.reactivex.rxjava3:rxandroid:${Versions.RX}"
-    const val RX_BINDING = "io.reactivex.rxjava3:rxandroid:${Versions.RX_BINDING}"
-    const val RX_JAVA = "io.reactivex.rxjava3:rxandroid:${Versions.RX_JAVA}"
+    const val RX_KOTLIN = "io.reactivex.rxjava3:rxkotlin:${Versions.RX}"
+    const val RX_BINDING = "com.jakewharton.rxbinding3:rxbinding:${Versions.RX_BINDING}"
+    const val RX_JAVA = "io.reactivex.rxjava3:rxjava:${Versions.RX}"
 }
 
 object Libraries {
@@ -94,6 +96,8 @@ object Libraries {
         "com.squareup.okhttp3:logging-interceptor:${Versions.OKHTTP}"
     const val PAGING_RUNTIME = ("androidx.paging:paging-runtime:${Versions.PAGING}")
     const val PAGING_GUAVA = ("androidx.paging:paging-guava:${Versions.PAGING}")
+    const val PAGING_TEST = "androidx.paging:paging-common:${Versions.PAGING}"
+
 }
 
 object UnitTest {

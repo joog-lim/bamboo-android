fd50308a75010e8f597f5ddec6bb1cc7680a6314
ckrudals <2004ckck@naver.com>
2022-01-18 20:08:10 +0900

    replace common package

--- domain/src/main/java/com/example/domain/usecease/common/AuthUseCase.kt
+++ domain/src/main/java/com/example/domain/usecease/common/AuthUseCase.kt
@@ -1,14 +1,14 @@
-package com.example.domain.usecease.user
+package com.example.domain.usecease.common
 
 import com.example.domain.base.BaseDataEntity
 import com.example.domain.base.BaseEntity
-import com.example.domain.model.user.response.SignEntity
-import com.example.domain.repository.UserRepository
+import com.example.domain.model.common.LoginEntity
+import com.example.domain.repository.CommonRepository
 import io.reactivex.rxjava3.core.Single
 
-class AuthUseCase(private val repository:UserRepository)  {
+class AuthUseCase(private val repository:CommonRepository)  {
 
-    fun postLogin(authorization: String): Single<BaseDataEntity<SignEntity>>{
+    fun postLogin(authorization: String): Single<BaseDataEntity<LoginEntity>>{
         return repository.postLogin(authorization)
     }
     fun deleteLogOut(authorization: String): Single<BaseEntity>{

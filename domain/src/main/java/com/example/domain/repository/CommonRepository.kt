4236182341735e6ba6fbe3f0b724eba6534c6f97
ckrudals <2004ckck@naver.com>
2022-01-18 20:08:27 +0900

    add login, logout func

--- domain/src/main/java/com/example/domain/repository/CommonRepository.kt
+++ domain/src/main/java/com/example/domain/repository/CommonRepository.kt
@@ -1,8 +1,12 @@
 package com.example.domain.repository
 
 import androidx.paging.PagingData
+import com.example.domain.base.BaseDataEntity
+import com.example.domain.base.BaseEntity
 import com.example.domain.model.admin.response.PostEntity
+import com.example.domain.model.common.LoginEntity
 import com.example.domain.model.user.TokenEntity
+import io.reactivex.rxjava3.core.Single
 import kotlinx.coroutines.flow.Flow
 
 interface CommonRepository {
@@ -15,7 +19,11 @@ interface CommonRepository {
         token: String,
         status: String,
 
-        ):Flow<PagingData<PostEntity>>
+        ): Flow<PagingData<PostEntity>>
+
+    fun postLogin(token: String): Single<BaseDataEntity<LoginEntity>>
+    fun deleteLogOut(authorization: String): Single<BaseEntity>
+
 
 }
 

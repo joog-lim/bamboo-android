a8bc9e9ac8509155ced8fb5517bb1d7b7eeb4f23
ckrudals <2004ckck@naver.com>
2022-01-18 21:06:51 +0900

    delete login, logout func

--- data/src/main/java/com/example/data/repository/user/UserRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/user/UserRepositoryImpl.kt
@@ -10,7 +10,6 @@ import com.example.domain.model.user.request.Report
 import com.example.domain.model.user.response.AlgorithmCreateEntity
 import com.example.domain.model.user.response.GetVerifyEntity
 import com.example.domain.model.user.response.ReportEntity
-import com.example.domain.model.user.response.SignEntity
 import com.example.domain.repository.UserRepository
 import io.reactivex.rxjava3.core.Single
 import javax.inject.Inject
@@ -18,34 +17,29 @@ import javax.inject.Inject
 class UserRepositoryImpl @Inject constructor(private val dataSource: UserDataSource) :
     UserRepository {
 
-    override suspend fun algorithmCreate(request: AlgorithmCreate): Single<BaseDataEntity<AlgorithmCreateEntity>> {
+    override  fun algorithmCreate(request: AlgorithmCreate): Single<BaseDataEntity<AlgorithmCreateEntity>> {
         return dataSource.algorithmCreate(request.toData()).map { it.toDomain() }
     }
 
-    override suspend fun getVerify(): Single<BaseDataEntity<GetVerifyEntity>> {
+    override  fun getVerify(): Single<BaseDataEntity<GetVerifyEntity>> {
         return dataSource.getVerify().map { it.toVerifyDomain() }
     }
 
-    override suspend fun patchReport(
+    override  fun patchAlgorithmReport(
         id: String,
         body: Report
     ): Single<BaseDataEntity<ReportEntity>> {
         return dataSource.patchReport(id, body.toData()).map { it.toReportDomain() }
     }
 
-    override suspend fun postLogin(authorization: String): Single<BaseDataEntity<SignEntity>> {
-        return dataSource.postLogin(authorization).map { it.toSignDomain() }
-    }
 
-    override suspend fun deleteLogOut(authorization: String): Single<BaseEntity> {
-        return dataSource.deleteLogOut(authorization).map { it.toDomain() }
-    }
 
-    override suspend fun postEmoji(authorization: String, body: EmojiEntity): Single<BaseEntity> {
+
+    override  fun postEmoji(authorization: String, body: EmojiEntity): Single<BaseEntity> {
         return dataSource.postEmoji(authorization, body.toData()).map { it.toDomain() }
     }
 
-    override suspend fun deleteEmoji(authorization: String, body: EmojiEntity): Single<BaseEntity> {
+    override  fun deleteEmoji(authorization: String, body: EmojiEntity): Single<BaseEntity> {
         return dataSource.deleteEmoji(authorization, body.toData()).map { it.toDomain() }
     }
 }

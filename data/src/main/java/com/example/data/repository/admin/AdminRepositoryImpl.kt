f4332a6c9f88aa812043294e2606ddae45e92fef
ckrudals <2004ckck@naver.com>
2022-01-21 23:56:21 +0900

    delete getAlgorithm func

--- data/src/main/java/com/example/data/repository/admin/AdminRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/admin/AdminRepositoryImpl.kt
@@ -1,15 +1,11 @@
 package com.example.data.repository.admin
 
-import com.example.data.mapper.admin.toAlgorithmDomain
 import com.example.data.mapper.toDomain
 import com.example.data.mapper.user.toData
-import com.example.domain.base.BaseDataEntity
 import com.example.domain.base.BaseEntity
 import com.example.domain.model.admin.request.AlgorithmModifyEntity
 import com.example.domain.model.admin.request.SetStatusEntity
-import com.example.domain.model.admin.response.AlgorithmEntity
 import com.example.domain.repository.AdminRepository
-import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.core.Single
 import javax.inject.Inject
 
@@ -36,15 +32,7 @@ class AdminRepositoryImpl @Inject constructor(
         return dataSource.deleteAlgorithm(token, id).map { it.toDomain() }
     }
 
-    override fun getAlgorithmPage(
-        token: String,
-        count: Int,
-        page: Int,
-        status: String
-    ): Flowable<BaseDataEntity<AlgorithmEntity>> {
-        return dataSource.getAlgorithmPage(token, count, page, status)
-            .map { it.toAlgorithmDomain() }
-    }
+
 }
 
 

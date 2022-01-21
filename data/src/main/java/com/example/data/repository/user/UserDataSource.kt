b2122d4767bd60669479adc153ce59a3bc13a1fb
ckrudals <2004ckck@naver.com>
2022-01-21 23:54:25 +0900

    delete login, logout commoon package 로 변경

--- data/src/main/java/com/example/data/repository/user/UserDataSource.kt
+++ data/src/main/java/com/example/data/repository/user/UserDataSource.kt
@@ -7,27 +7,24 @@ import com.example.data.model.user.request.EmojiRequest
 import com.example.data.model.user.request.ReportRequest
 import com.example.data.model.user.response.GetVerifyResponse
 import com.example.data.model.user.response.ReportResponse
-import com.example.data.model.user.response.SignResponse
 import com.example.data.model.user.response.AlgorithmCreateResponse
 import io.reactivex.rxjava3.core.Single
 
 
 interface UserDataSource {
-    suspend fun algorithmCreate(request: AlgorithmCreateRequest): Single<BaseDataResponse<AlgorithmCreateResponse>>
-    suspend fun getVerify(): Single<BaseDataResponse<GetVerifyResponse>>
-    suspend fun patchReport(
+     fun algorithmCreate(request: AlgorithmCreateRequest): Single<BaseDataResponse<AlgorithmCreateResponse>>
+     fun getVerify(): Single<BaseDataResponse<GetVerifyResponse>>
+     fun patchReport(
         id: String,
         body: ReportRequest,
     ): Single<BaseDataResponse<ReportResponse>>
 
-    suspend fun postLogin(authorization: String): Single<BaseDataResponse<SignResponse>>
-    suspend fun deleteLogOut(authorization: String): Single<BaseResponse>
-    suspend fun postEmoji(
+     fun postEmoji(
         authorization: String,
         body: EmojiRequest
     ): Single<BaseResponse>
 
-    suspend fun deleteEmoji(
+     fun deleteEmoji(
         authorization: String,
         body: EmojiRequest
     ): Single<BaseResponse>
--- data/src/main/java/com/example/data/repository/user/UserDataSourceImpl.kt
+++ data/src/main/java/com/example/data/repository/user/UserDataSourceImpl.kt
@@ -8,46 +8,40 @@ import com.example.data.model.user.request.EmojiRequest
 import com.example.data.model.user.request.ReportRequest
 import com.example.data.model.user.response.GetVerifyResponse
 import com.example.data.model.user.response.ReportResponse
-import com.example.data.model.user.response.SignResponse
+import com.example.data.model.common.LoginResponse
 import com.example.data.model.user.response.AlgorithmCreateResponse
 import com.example.data.network.user.UserApi
 import io.reactivex.rxjava3.core.Single
 
 class UserDataSourceImpl(override val service: UserApi) : UserDataSource,
     BaseDataSource<UserApi>() {
-    override suspend fun algorithmCreate(request: AlgorithmCreateRequest): Single<BaseDataResponse<AlgorithmCreateResponse>> {
+    override  fun algorithmCreate(request: AlgorithmCreateRequest): Single<BaseDataResponse<AlgorithmCreateResponse>> {
         return service.algorithmCreate(request)
     }
 
-    override suspend fun getVerify(): Single<BaseDataResponse<GetVerifyResponse>> {
+    override  fun getVerify(): Single<BaseDataResponse<GetVerifyResponse>> {
 
         return service.getVerify()
     }
 
-    override suspend fun patchReport(
+    override  fun patchReport(
         id: String,
         body: ReportRequest
     ): Single<BaseDataResponse<ReportResponse>> {
-        return service.patchReport(id, body)
+        return service.patchAlgorithmReport(id, body)
 
     }
 
-    override suspend fun postLogin(authorization: String): Single<BaseDataResponse<SignResponse>> {
-        return service.postLogin(authorization)
-    }
 
-    override suspend fun deleteLogOut(authorization: String): Single<BaseResponse> {
-        return service.deleteLogOut(authorization)
-    }
 
-    override suspend fun postEmoji(
+    override  fun postEmoji(
         authorization: String,
         body: EmojiRequest
     ): Single<BaseResponse> {
         return service.postEmoji(authorization, body)
     }
 
-    override suspend fun deleteEmoji(
+    override  fun deleteEmoji(
         authorization: String,
         body: EmojiRequest
     ): Single<BaseResponse> {

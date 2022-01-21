dd9a815a18be034704e9e0133858c80404866ba7
ckrudals <2004ckck@naver.com>
2022-01-21 23:55:10 +0900

    add loginMapper

--- data/src/main/java/com/example/data/mapper/BaseMapper.kt
+++ data/src/main/java/com/example/data/mapper/BaseMapper.kt
@@ -1,7 +1,10 @@
 package com.example.data.mapper
 
+import com.example.data.base.BaseDataResponse
 import com.example.data.base.BaseResponse
+import com.example.domain.base.BaseDataEntity
 import com.example.domain.base.BaseEntity
+import com.example.domain.model.common.LoginEntity
 
 fun BaseResponse.toDomain(): BaseEntity {
     return BaseEntity(
@@ -9,5 +12,9 @@ fun BaseResponse.toDomain(): BaseEntity {
         this.message
     )
 }
+fun <T> BaseDataResponse<T>.toLoginDomain(): BaseDataEntity<LoginEntity> {
+    return BaseDataEntity(this.status, this.message, this.data as LoginEntity)
+}
+
 
 

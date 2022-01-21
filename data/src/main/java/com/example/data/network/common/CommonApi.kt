c45398a85647303d0776e92de41d746ab092e167
ckrudals <2004ckck@naver.com>
2022-01-21 23:57:43 +0900

    add login, logout func

--- data/src/main/java/com/example/data/network/common/CommonApi.kt
+++ data/src/main/java/com/example/data/network/common/CommonApi.kt
@@ -1,11 +1,12 @@
 package com.example.data.network.common
 
 import com.example.data.base.BaseDataResponse
+import com.example.data.base.BaseResponse
 import com.example.data.model.admin.response.AlgorithmResponse
+import com.example.data.model.common.LoginResponse
 import io.reactivex.rxjava3.core.Flowable
-import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.Query
+import io.reactivex.rxjava3.core.Single
+import retrofit2.http.*
 
 interface CommonApi {
 
@@ -17,5 +18,16 @@ interface CommonApi {
         @Query("page") page: Int,
         @Query("status") status: String
     ): Flowable<BaseDataResponse<AlgorithmResponse>>
+
+    @POST("login")
+    fun postLogin(
+        @Header("Authorization") Authorization: String,
+    ): Single<BaseDataResponse<LoginResponse>>
+    @DELETE("logout")
+    fun deleteLogOut(
+        @Header("Authorization") authorization: String
+    ): Single<BaseResponse>
+
+
 
 }

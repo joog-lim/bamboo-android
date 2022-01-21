df06bbd4bfc36792da98ae828475df14372ff3a9
ckrudals <2004ckck@naver.com>
2022-01-21 23:53:07 +0900

    module 지우면서 생긴 파일

--- app/src/main/java/com/example/app/App.kt
+++
@@ -1,8 +0,0 @@
-package com.example.app
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class App : Application() {
-}
--- app/src/main/java/com/example/app/di/UseCaseModule.kt
+++
@@ -1,4 +0,0 @@
-package com.example.app.di
-
-class UseCaseModule {
-}

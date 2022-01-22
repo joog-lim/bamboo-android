3d6f83bfd0e8841f100358f694795d2c9693d0ff
ckrudals <2004ckck@naver.com>
2022-01-23 01:37:21 +0900

    update fragment -> activity

--- presentation/src/main/java/com/example/presentation/view/SignInFragment.kt
+++ presentation/src/main/java/com/example/presentation/view/SignInFragment.kt
@@ -2,15 +2,21 @@ package com.example.presentation.view
 
 import android.app.Activity
 import android.content.Intent
+import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.fragment.app.viewModels
+import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
-import com.example.base.base.base.BaseFragment
+import com.example.base.base.base.BaseActivity
+import com.example.data.utils.DataStoreManager
 import com.example.presentation.R
 import com.example.presentation.databinding.FragmentSignInBinding
+import com.example.presentation.view.admin.AdminActivity
+import com.example.presentation.view.admin.AuthViewModel
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -19,51 +25,45 @@ import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 //todo
 // base 모듈 만들어서 login, splash  빼고 옮겨야함
 @AndroidEntryPoint
-class SignInFragment : BaseFragment<FragmentSignInBinding>(R.layout.fragment_sign_in) {
-    private lateinit var getGoogleLogin: ActivityResultLauncher<Intent>
+class SignInFragment : BaseActivity<FragmentSignInBinding>(R.layout.fragment_sign_in) {
+    @Inject
+    lateinit var dataStore: DataStoreManager
+    private val getGoogleLogin =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            Log.d("TAG", "${result.resultCode}: ")
+                val intent: Intent = result.data!!
+                val task: Task<GoogleSignInAccount> =
+                    GoogleSignIn.getSignedInAccountFromIntent(intent)
+                try {
+                    val account = task.getResult(ApiException::class.java)
+                    viewModel.postLogin(account.idToken.toString())
+                } catch (e: ApiException) {
+                    Log.d("TAG", "onCreate: ${e.message}")
+                    throw e
+                }
+
+
+        }
     private var mGoogleSignInClient: GoogleSignInClient? = null
     private val viewModel: AuthViewModel by viewModels()
-
-    override fun FragmentSignInBinding.onCreateView() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         binding.fragment = this@SignInFragment
-        GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestEmail()
             .requestIdToken(getString(R.string.client_id))
-            .build().let {
-                mGoogleSignInClient = GoogleSignIn.getClient(requireContext(), it)
-            }
-        getGoogleLogin =
+            .build()
 
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                if (result.resultCode == Activity.RESULT_OK) {
-                    val intent: Intent = result.data!!
-                    val task: Task<GoogleSignInAccount> =
-                        GoogleSignIn.getSignedInAccountFromIntent(intent)
-                    try {
-                        val account = task.getResult(ApiException::class.java)
-                        viewModel.postLogin(account.idToken!!)
-                    } catch (e: ApiException) {
-                        throw e
-                    }
-
-                } else {
-                    Toast.makeText(
-                        requireContext(),
-                        "로그인 실패 에러코드 ${result.resultCode}",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-    }
-
-    override fun FragmentSignInBinding.onViewCreated() {
+        mGoogleSignInClient = GoogleSignIn.getClient(this@SignInFragment, gso)
 
         with(viewModel) {
-            isLoading.observe(viewLifecycleOwner) {
+            isLoading.observe(this@SignInFragment) {
                 if (it) {
                     binding.progressBar.visibility = View.VISIBLE
                 } else {
@@ -71,25 +71,25 @@ class SignInFragment : BaseFragment<FragmentSignInBinding>(R.layout.fragment_sig
 
                 }
             }
-            isLoginResult.observe(viewLifecycleOwner) {
+            isLoginResult.observe(this@SignInFragment) {
                 if (it?.isAdmin == true) {
-                    startActivity(Intent(requireContext(), MainActivity::class.java))
-//                    saveToken(it.accessToken)
+                    startActivity(Intent(this@SignInFragment, MainActivity::class.java))
+                    saveToken(it.accessToken)
                 } else {
-                    startActivity(Intent(requireContext(), AdminActivity::class.java))
+                    startActivity(Intent(this@SignInFragment, AdminActivity::class.java))
                 }
             }
-            isFailure.observe(viewLifecycleOwner) {
-                Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
+            isFailure.observe(this@SignInFragment) {
+                Toast.makeText(this@SignInFragment, it, Toast.LENGTH_SHORT).show()
             }
         }
     }
-//   private fun saveToken(token:String)=lifecycleScope.launch { dataStore.saveToken(token) }
+
+
+    private fun saveToken(token: String) = lifecycleScope.launch { dataStore.saveToken(token) }
 
     fun startGoogleLogin() {
-        binding.loginBtn.setOnClickListener {
-            getGoogleLogin.launch(mGoogleSignInClient!!.signInIntent)
-        }
+        getGoogleLogin.launch(mGoogleSignInClient!!.signInIntent)
     }
 
 }

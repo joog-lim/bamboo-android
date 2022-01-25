d92ac4c3d2dd4dd30b761f4a2b6e5346a9c23e4c
ckrudals <2004ckck@naver.com>
2022-01-25 18:41:56 +0900

    add success, update status -> code value

--- data/src/main/java/com/study/data/base/BaseDataResponse.kt
+++ data/src/main/java/com/study/data/base/BaseDataResponse.kt
@@ -1,3 +1,3 @@
 package com.study.data.base
 
-data class BaseDataResponse<T>(val status: Int, val message: String, val data: T)
+data class BaseDataResponse<T>(val success:Boolean, val code: String, val message: String?, val `data`: T?)

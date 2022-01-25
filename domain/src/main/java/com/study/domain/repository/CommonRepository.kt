0e3b6fdcca92f7d14a6c6b6731bb07b60558836e
ckrudals <2004ckck@naver.com>
2022-01-25 18:47:45 +0900

    delete getAlgorithm func

--- domain/src/main/java/com/study/domain/repository/CommonRepository.kt
+++ domain/src/main/java/com/study/domain/repository/CommonRepository.kt
@@ -1,9 +1,7 @@
 package com.study.domain.repository
 
-import androidx.paging.PagingData
 import com.study.domain.base.BaseDataEntity
 import com.study.domain.base.BaseEntity
-import com.study.domain.model.admin.response.PostEntity
 import com.study.domain.model.common.LoginEntity
 import com.study.domain.model.user.TokenEntity
 import io.reactivex.rxjava3.core.Single
@@ -15,11 +13,7 @@ interface CommonRepository {
     suspend fun saveToken(token: String)
     var readToken: Flow<TokenEntity>
 
-    fun getAlgorithmPagingSource(
-        token: String,
-        status: String,
 
-        ): Flow<PagingData<PostEntity>>
 
     fun postLogin(token: String): Single<BaseDataEntity<LoginEntity>>
     fun deleteLogOut(authorization: String): Single<BaseEntity>

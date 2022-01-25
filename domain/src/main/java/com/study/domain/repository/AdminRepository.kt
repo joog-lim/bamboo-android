17131d8db21e9c01cc79803244c7fc507aa9e090
ckrudals <2004ckck@naver.com>
2022-01-25 18:46:47 +0900

    add getAlgorithm api

--- domain/src/main/java/com/study/domain/repository/AdminRepository.kt
+++ domain/src/main/java/com/study/domain/repository/AdminRepository.kt
@@ -1,9 +1,12 @@
 package com.study.domain.repository
 
+import androidx.paging.PagingData
 import com.study.domain.base.BaseEntity
 import com.study.domain.model.admin.request.AlgorithmModifyEntity
 import com.study.domain.model.admin.request.SetStatusEntity
+import com.study.domain.model.common.algorithm.ResultEntity
 import io.reactivex.rxjava3.core.Single
+import kotlinx.coroutines.flow.Flow
 
 interface AdminRepository {
 
@@ -21,5 +24,9 @@ interface AdminRepository {
 
     fun deleteAlgorithm(token: String, id: String): Single<BaseEntity>
 
+    fun getAlgorithmPagingSource(
+        token: String,
+        status: String,
 
+        ): Flow<PagingData<ResultEntity>>
 }
--- domain/src/main/java/com/study/domain/usecease/admin/AlgorithmAdminUseCase.kt
+++ domain/src/main/java/com/study/domain/usecease/admin/AlgorithmAdminUseCase.kt
@@ -1,13 +1,23 @@
 package com.study.domain.usecease.admin
 
+import androidx.paging.PagingData
 import com.study.domain.base.BaseEntity
 import com.study.domain.model.admin.request.AlgorithmModifyEntity
 import com.study.domain.model.admin.request.SetStatusEntity
+import com.study.domain.model.common.algorithm.DataEntity
+import com.study.domain.model.common.algorithm.ResultEntity
 import com.study.domain.repository.AdminRepository
 import io.reactivex.rxjava3.core.Single
+import kotlinx.coroutines.flow.Flow
 
 class AlgorithmAdminUseCase(private val repository: AdminRepository) {
+    fun getAlgorithmPagingSource(
+        token: String,
+        status: String,
 
+        ): Flow<PagingData<ResultEntity>> {
+        return repository.getAlgorithmPagingSource(token, status)
+    }
     fun patchStatusAlgorithm(
         token: String,
         id: String,

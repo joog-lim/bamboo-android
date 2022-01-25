c87b4e5302ab92634c8bdea0fbfbc544d897736b
ckrudals <2004ckck@naver.com>
2022-01-25 18:47:08 +0900

    add userGetAlgorithm api

--- domain/src/main/java/com/study/domain/repository/UserRepository.kt
+++ domain/src/main/java/com/study/domain/repository/UserRepository.kt
@@ -1,30 +1,37 @@
 package com.study.domain.repository
 
+import androidx.paging.PagingData
 import com.study.domain.base.BaseDataEntity
 import com.study.domain.base.BaseEntity
+import com.study.domain.model.common.algorithm.DataEntity
+import com.study.domain.model.common.algorithm.ResultEntity
 import com.study.domain.model.user.request.AlgorithmCreate
 import com.study.domain.model.user.request.EmojiEntity
 import com.study.domain.model.user.request.Report
-import com.study.domain.model.user.response.AlgorithmCreateEntity
-import com.study.domain.model.user.response.GetVerifyEntity
-import com.study.domain.model.user.response.ReportEntity
+import com.study.domain.model.user.response.*
 import io.reactivex.rxjava3.core.Single
+import kotlinx.coroutines.flow.Flow
 
 interface UserRepository {
-     fun algorithmCreate(request: AlgorithmCreate): Single<BaseDataEntity<AlgorithmCreateEntity>>
-     fun getVerify(): Single<BaseDataEntity<GetVerifyEntity>>
-     fun patchAlgorithmReport(
+    fun algorithmCreate(request: AlgorithmCreate): Single<BaseDataEntity<AlgorithmCreateEntity>>
+    fun getVerify(): Single<BaseDataEntity<GetVerifyEntity>>
+    fun patchAlgorithmReport(
         id: String,
         body: Report,
     ): Single<BaseDataEntity<ReportEntity>>
 
-     fun postEmoji(
+    fun postEmoji(
         authorization: String,
         body: EmojiEntity
     ): Single<BaseEntity>
 
-     fun deleteEmoji(
+    fun deleteEmoji(
         authorization: String,
         body: EmojiEntity
     ): Single<BaseEntity>
+
+    fun getAlgorithmPagingSource(
+        token: String,
+
+        ): Flow<PagingData<ResultEntity>>
 }
--- domain/src/main/java/com/study/domain/usecease/user/AlgorithmUserUseCase.kt
+++ domain/src/main/java/com/study/domain/usecease/user/AlgorithmUserUseCase.kt
@@ -1,15 +1,23 @@
 package com.study.domain.usecease.user
 
+import androidx.paging.PagingData
 import com.study.domain.base.BaseDataEntity
+import com.study.domain.model.common.algorithm.ResultEntity
 import com.study.domain.model.user.request.AlgorithmCreate
 import com.study.domain.model.user.request.Report
-import com.study.domain.model.user.response.AlgorithmCreateEntity
-import com.study.domain.model.user.response.GetVerifyEntity
-import com.study.domain.model.user.response.ReportEntity
+import com.study.domain.model.user.response.*
 import com.study.domain.repository.UserRepository
 import io.reactivex.rxjava3.core.Single
+import kotlinx.coroutines.flow.Flow
 
 class AlgorithmUserUseCase(private val repository: UserRepository) {
+    fun getAlgorithmPagingSource(
+        token: String,
+
+        ): Flow<PagingData<ResultEntity>> {
+        return repository.getAlgorithmPagingSource(token)
+    }
+
     fun algorithmCreate(request: AlgorithmCreate): Single<BaseDataEntity<AlgorithmCreateEntity>> {
         return repository.algorithmCreate(request)
     }

d81e66e7ad3d2d024a36c357fe0dac729c1a628f
ckrudals <2004ckck@naver.com>
2022-01-27 15:35:08 +0900

    delete getAlgorithmPagingSource func

--- data/src/main/java/com/study/data/repository/common/CommonRepositoryImpl.kt
+++ data/src/main/java/com/study/data/repository/common/CommonRepositoryImpl.kt
@@ -1,21 +1,17 @@
 package com.study.data.repository.common
 
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.PagingData
-import androidx.paging.map
+import android.util.Log
+import com.study.data.base.BaseDataResponse
 import com.study.data.mapper.toDomain
 import com.study.data.mapper.toLoginDomain
-import com.study.data.network.common.CommonApi
-import com.study.data.paging.AlgorithmPagingSource
-import io.reactivex.rxjava3.core.Single
+import com.study.data.model.common.LoginResponse
 import com.study.domain.base.BaseDataEntity
 import com.study.domain.base.BaseEntity
-import com.study.domain.model.admin.response.PostEntity
 import com.study.domain.model.common.LoginEntity
 import com.study.domain.model.user.TokenEntity
 import com.study.domain.repository.CommonRepository
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -23,7 +19,6 @@ import javax.inject.Inject
 
 class CommonRepositoryImpl @Inject constructor(
     private val dataSource: CommonDataSourceImpl,
-    private val api: CommonApi,
 ) : CommonRepository {
 
 
@@ -37,22 +32,13 @@ class CommonRepositoryImpl @Inject constructor(
             dataSource.readToken = value.map { it.toDomain() }
         }
 
-    override fun getAlgorithmPagingSource(
-        token: String,
-        status: String
-    ): Flow<PagingData<PostEntity>> {
-        return Pager(config = PagingConfig(pageSize = 20),
-            pagingSourceFactory = { AlgorithmPagingSource(api, token, status) }
-        ).flow.map { it ->
-            it.map { it.toDomain() }
-        }
-    }
 
     override fun postLogin(token: String): Single<BaseDataEntity<LoginEntity>> {
         return dataSource.postLogin(token).observeOn(AndroidSchedulers.mainThread())
-            .subscribeOn(Schedulers.io()).map {
+            .subscribeOn(Schedulers.io())?.map {
+                Log.d("CommonRepositoryImpl", "postLogin: ${it}")
                 it.toLoginDomain()
-            }
+            } as Single<BaseDataEntity<LoginEntity>>
 
     }
 
@@ -61,3 +47,4 @@ class CommonRepositoryImpl @Inject constructor(
     }
 
 }
+

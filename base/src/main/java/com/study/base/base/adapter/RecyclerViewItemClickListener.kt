a5061228c5a500fd7bdefe4efc554a2faf98a349
ckrudals <2004ckck@naver.com>
2022-01-27 15:31:16 +0900

    add adapter dialog clickListener func

--- base/src/main/java/com/study/base/base/adapter/RecyclerViewItemClickListener.kt
+++ base/src/main/java/com/study/base/base/adapter/RecyclerViewItemClickListener.kt
@@ -3,3 +3,7 @@ package com.study.base.base.adapter
 interface RecyclerViewItemClickListener<T> {
     fun onclick(data: T): Unit
 }
+
+interface RecyclerViewDialogItemClickListener<T> {
+    fun oncDialogClick(data: T, state: String): Unit
+}

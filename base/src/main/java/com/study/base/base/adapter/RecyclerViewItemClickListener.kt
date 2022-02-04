af4d5201e6c4fbc5c4c16c17ced7584ab29d2b9c
ckrudals <2004ckck@naver.com>
2022-02-04 23:51:42 +0900

    #18 update interface 하나로 합침

--- base/src/main/java/com/study/base/base/adapter/RecyclerViewItemClickListener.kt
+++ base/src/main/java/com/study/base/base/adapter/RecyclerViewItemClickListener.kt
@@ -1,9 +1,8 @@
 package com.study.base.base.adapter
 
-interface RecyclerViewItemClickListener<T> {
-    fun onclick(data: T): Unit
-}
 
-interface RecyclerViewDialogItemClickListener<T> {
-    fun oncDialogClick(data: T, state: String): Unit
+interface RecyclerViewItemClickListener<T> {
+    fun onStateClick(data: T, state: String): Unit
+    fun onLeafClick(data: T): Unit
+
 }

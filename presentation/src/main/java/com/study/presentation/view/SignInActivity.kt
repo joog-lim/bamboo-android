fe527e561209cff751358ef6f8b38241ecc3613e
ckrudals <2004ckck@naver.com>
2022-02-15 03:50:58 +0900

    rename SignInFragment -> SignInActivity

--- presentation/src/main/java/com/study/presentation/view/SignInActivity.kt
+++ presentation/src/main/java/com/study/presentation/view/SignInActivity.kt
@@ -19,31 +19,36 @@ import com.google.android.gms.tasks.Task
 import com.study.base.base.base.BaseActivity
 import com.study.data.utils.DataStoreManager
 import com.study.presentation.R
-import com.study.presentation.databinding.FragmentSignInBinding
+import com.study.presentation.databinding.ActivitySignInBinding
 import com.study.presentation.view.admin.AdminActivity
 import com.study.presentation.view.admin.AuthViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class SignInFragment : BaseActivity<FragmentSignInBinding>(R.layout.fragment_sign_in) {
+class SignInActivity : BaseActivity<ActivitySignInBinding>(R.layout.activity_sign_in) {
     @Inject
     lateinit var dataStore: DataStoreManager
 
     private var mGoogleSignInClient: GoogleSignInClient? = null
     private val getGoogleLogin =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
-            Log.d("TAG", "${result.resultCode}: ")
-            val intent: Intent = result.data!!
-            val task: Task<GoogleSignInAccount> =
-                GoogleSignIn.getSignedInAccountFromIntent(intent)
-            try {
-                val account = task.getResult(ApiException::class.java)
-                viewModel.postLogin(account?.idToken.toString())
-            } catch (e: ApiException) {
-                Log.d("TAG", "onCreate: ${e.message}")
-                throw e
+            if (result.resultCode == RESULT_OK) {
+                try {
+
+                    val intent: Intent = result.data!!
+                    val task: Task<GoogleSignInAccount> =
+                        GoogleSignIn.getSignedInAccountFromIntent(intent)
+                    val account = task.getResult(ApiException::class.java)
+                    viewModel.postLogin(account?.idToken.toString())
+                } catch (e: ApiException) {
+                    Log.d("TAG", "onCreate: ${e.message}")
+                    throw e
+                }
+            } else {
+                Log.d("TAG", "${result.resultCode}: ")
             }
 
 
@@ -51,19 +56,19 @@ class SignInFragment : BaseActivity<FragmentSignInBinding>(R.layout.fragment_sig
     private val viewModel: AuthViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding.fragment = this@SignInFragment
+        binding.fragment = this@SignInActivity
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestEmail()
             .requestIdToken(getString(R.string.client_id))
             .build()
 
-        mGoogleSignInClient = GoogleSignIn.getClient(this@SignInFragment, gso)
+        mGoogleSignInClient = GoogleSignIn.getClient(this@SignInActivity, gso)
 
 
 
         with(viewModel) {
-            isLoading.observe(this@SignInFragment) {
+            isLoading.observe(this@SignInActivity) {
                 if (it) {
                     binding.progressBar.visibility = View.VISIBLE
                 } else {
@@ -71,27 +76,37 @@ class SignInFragment : BaseActivity<FragmentSignInBinding>(R.layout.fragment_sig
 
                 }
             }
-            isLoginResult.observe(this@SignInFragment) {
+            isLoginResult.observe(this@SignInActivity) {
+                Log.d("TAG", "onCreate: ${it}")
+                saveToken(it?.accessToken.toString())
                 if (it?.isAdmin == true) {
-                    startActivity(Intent(this@SignInFragment, MainActivity::class.java))
-                    saveToken(it.accessToken)
+                    startActivity(Intent(this@SignInActivity, AdminActivity::class.java))
                 } else {
-                    startActivity(Intent(this@SignInFragment, AdminActivity::class.java))
+                    startActivity(Intent(this@SignInActivity, MainActivity::class.java))
                 }
             }
-            isFailure.observe(this@SignInFragment) {
-                Toast.makeText(this@SignInFragment, it, Toast.LENGTH_SHORT).show()
+            isFailure.observe(this@SignInActivity) {
+                Log.d("TAG", "onCreate: ${it}")
+                Toast.makeText(this@SignInActivity, it.toString(), Toast.LENGTH_SHORT).show()
             }
         }
     }
 
 
     private fun saveToken(token: String) = lifecycleScope.launch { dataStore.saveToken(token) }
-    fun guestOnClick() = startActivity(Intent(this, MainActivity::class.java))
+    fun guestOnClick() {
+        lifecycleScope.launch {
+            dataStore.deleteToken(dataStore.getToken())
+        }
+        startActivity(Intent(this, MainActivity::class.java))
+    }
 
 
     fun startGoogleLogin() {
-        getGoogleLogin.launch(mGoogleSignInClient?.signInIntent)
+        lifecycleScope.launch {
+            getGoogleLogin.launch(mGoogleSignInClient?.signInIntent)
+            delay(3000)
+        }
     }
 
 }

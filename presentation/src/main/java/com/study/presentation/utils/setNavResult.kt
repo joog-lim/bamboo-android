197c65c391cce5c1da349a79f116a40374ceacca
ckrudals <2004ckck@naver.com>
2022-02-15 03:58:30 +0900

    add setNavResult extension func

--- presentation/src/main/java/com/study/presentation/utils/setNavResult.kt
+++ presentation/src/main/java/com/study/presentation/utils/setNavResult.kt
@@ -1,9 +1,5 @@
 package com.study.presentation.utils
 
-import android.app.Activity
-import android.content.Context
-import android.view.View
-import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.study.presentation.utils.DiaryUtil.Companion.DIALOG_RESULT_KEY
@@ -14,7 +10,3 @@ fun <T> Fragment.setNavResult(key: String = DIALOG_RESULT_KEY, data: T) {
         }
     }
 
-fun hideKeyboardFrom(context: Context, view: View) {
-    val imm = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-    imm.hideSoftInputFromWindow(view.windowToken, 0);
-}

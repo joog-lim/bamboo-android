89d01353ac0374006dee972c1798a4235ee3c1e6
ckrudals <2004ckck@naver.com>
2022-02-15 03:25:28 +0900

    add patchAlgorithm func

--- domain/src/main/java/com/study/domain/repository/CommonRepository.kt
+++ domain/src/main/java/com/study/domain/repository/CommonRepository.kt
@@ -2,6 +2,7 @@ package com.study.domain.repository
 
 import com.study.domain.base.BaseDataEntity
 import com.study.domain.base.BaseEntity
+import com.study.domain.model.admin.request.SetStatusEntity
 import com.study.domain.model.common.LoginEntity
 import com.study.domain.model.user.TokenEntity
 import io.reactivex.rxjava3.core.Single
@@ -17,7 +18,11 @@ interface CommonRepository {
 
     fun postLogin(token: String): Single<BaseDataEntity<LoginEntity>>
     fun deleteLogOut(authorization: String): Single<BaseEntity>
-
+    fun patchStatusAlgorithm(
+        token: String,
+        id: String,
+        body: SetStatusEntity
+    ): Single<BaseEntity>
 
 }
 

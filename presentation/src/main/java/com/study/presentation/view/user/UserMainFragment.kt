c728e6c96670f3d5f582965e7e41ba746b1ca92b
ckrudals <2004ckck@naver.com>
2022-02-15 03:39:29 +0900

    refactor UserMainFragment.kt

--- presentation/src/main/java/com/study/presentation/view/user/UserMainFragment.kt
+++ presentation/src/main/java/com/study/presentation/view/user/UserMainFragment.kt
@@ -1,16 +1,26 @@
 package com.study.presentation.view.user
 
-import android.content.Context
-import android.view.View
-import androidx.activity.OnBackPressedCallback
+import android.content.res.Resources
+import android.util.Log
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import com.study.base.base.adapter.PostLoadingAdapter
+import com.study.base.base.adapter.RecyclerViewItemClickListener
 import com.study.base.base.base.BaseFragment
+import com.study.base.base.utils.LinearLayoutManagerWrapper
+import com.study.domain.model.common.algorithm.ResultEntity
+import com.study.domain.model.user.request.EmojiEntity
 import com.study.presentation.R
-import com.study.presentation.adapter.UserHomeItemAdapter
+import com.study.presentation.adapter.AlgorithmAdapter
+import com.study.presentation.adapter.STATUS
 import com.study.presentation.databinding.FragmentUserMainBinding
+import com.study.presentation.view.admin.AuthViewModel
 import com.study.presentation.view.user.viewmodel.AlgorithmViewModel
 import com.study.presentation.view.user.viewmodel.EmojiViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,60 +28,158 @@ import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class UserMainFragment : BaseFragment<FragmentUserMainBinding>(R.layout.fragment_user_main) {
+class UserMainFragment : BaseFragment<FragmentUserMainBinding>(R.layout.fragment_user_main),
+    RecyclerViewItemClickListener<ResultEntity> {
 
     private val mainViewModel by viewModels<AlgorithmViewModel>()
+    private val authViewModel by viewModels<AuthViewModel>()
     private val userViewModel by viewModels<EmojiViewModel>()
 
-    private lateinit var callback: OnBackPressedCallback
 
-    private val userAlgorithmAdapter: UserHomeItemAdapter by lazy { UserHomeItemAdapter() }
+    private val userAlgorithmAdapter: AlgorithmAdapter by lazy {
+        AlgorithmAdapter(
+            this,
+            STATUS.USER
+        )
+    }
+
     override fun FragmentUserMainBinding.onCreateView() {
-        binding.activity = this@UserMainFragment
-    }
+        binding.fragment = this@UserMainFragment
 
-
-
-    fun addPostBtnClick(view: View) {
-        view.findNavController().navigate(R.id.action_userMainFragment_to_postCreateFragment)
-    }
-
-
-    private fun initRecyclerView() {
-
-        binding.postRecyclerView.apply {
-            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
-            adapter = userAlgorithmAdapter
-        }
-
-
-    }
-
-    private fun getAlgorithmList() {
-        lifecycleScope.launch {
-            mainViewModel.getAlgorithm(token, "ACCEPTED").collectLatest {
-                (userAlgorithmAdapter).submitData(
-                    it
-                )
-            }
-        }
     }
 
 
     override fun FragmentUserMainBinding.onViewCreated() {
         initRecyclerView()
         getAlgorithmList()
+        stateAdapter()
+        refreshButtonAndLayout()
+
+        with(userViewModel) {
+            isSuccess.observe(viewLifecycleOwner) {
+                userAlgorithmAdapter.refresh()
+            }
+            isFailure.observe(viewLifecycleOwner) {
+                Snackbar.make(binding.root, "사용하시려면 로그인을 해주세요.", Snackbar.LENGTH_SHORT)
+                    .setAction("OK") {
+
+                    }.show()
+            }
+        }
+    }
+
+    fun postBtnClick() {
+        findNavController().navigate(R.id.action_userMainFragment_to_postCreateFragment)
+    }
+
+    private fun refreshButtonAndLayout() {
+        binding.refreshLayout.setOnRefreshListener {
+
+            userAlgorithmAdapter.refresh()
+            binding.refreshLayout.isRefreshing = false
+
+        }
+
 
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        callback = object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
+    private fun initRecyclerView() {
 
+        val linearLayoutManagerWrapper =
+            context?.let { LinearLayoutManagerWrapper(it, LinearLayoutManager.VERTICAL, false) }
+
+        binding.postRecyclerView.apply {
+            adapter = userAlgorithmAdapter
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            this.adapter = userAlgorithmAdapter.withLoadStateHeaderAndFooter(
+                header = PostLoadingAdapter { userAlgorithmAdapter.retry() },
+                footer = PostLoadingAdapter { userAlgorithmAdapter.retry() },
+            )
+            this.layoutManager = linearLayoutManagerWrapper
+        }
+
+
+    }
+
+    fun retrySetButton() {
+        userAlgorithmAdapter.retry()
+    }
+
+    private fun getAlgorithmList() {
+        lifecycleScope.launch {
+            mainViewModel.getAlgorithm(authViewModel.getToken()).collectLatest {
+                userAlgorithmAdapter.submitData(
+                    it
+                )
             }
         }
-        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
+
+    private fun stateAdapter() {
+
+        userAlgorithmAdapter.addLoadStateListener { combinedLoadStates ->
+
+
+            when (combinedLoadStates.source.refresh) {
+                is LoadState.Error -> {
+                    binding.errorRetryBtn.isVisible = true
+                    binding.errorText.isVisible = true
+                    binding.errorImg.isVisible = true
+                    binding.errorNText.isVisible = true
+                    binding.postRecyclerView.hideShimmer()
+
+                }
+                is LoadState.Loading -> {
+                    with(binding) {
+                        postRecyclerView.showShimmer()
+                        errorRetryBtn.isVisible = false
+                        errorText.isVisible = false
+                        errorNText.isVisible = false
+                        errorImg.isVisible = false
+                        notFoundImg.isVisible = false
+                        notFoundText.isVisible = false
+                    }
+
+                }
+                is LoadState.NotLoading -> {
+                    binding.postRecyclerView.hideShimmer()
+                    binding.notFoundImg.isVisible =
+                        combinedLoadStates.append.endOfPaginationReached and (userAlgorithmAdapter.itemCount < 1)
+                    binding.notFoundText.isVisible =
+                        combinedLoadStates.append.endOfPaginationReached and (userAlgorithmAdapter.itemCount < 1)
+
+                }
+
+            }
+
+
+        }
+
+    }
+
+
+    override fun onStateClick(data: ResultEntity, state: String) {
+
+        val action =
+            UserMainFragmentDirections.actionUserMainFragmentToDeclarationFragment(data.idx)
+        findNavController().navigate(action)
+
+    }
+
+    override fun onLeafClick(data: ResultEntity) {
+
+        if (data.isClicked) {
+                lifecycleScope.launch {
+                userViewModel.deleteEmoji(authViewModel.getToken(), EmojiEntity(data.idx))
+            }
+
+        } else {
+            lifecycleScope.launch {
+                userViewModel.postEmoji(authViewModel.getToken(), EmojiEntity(data.idx))
+            }
+        }
+
     }
 
 

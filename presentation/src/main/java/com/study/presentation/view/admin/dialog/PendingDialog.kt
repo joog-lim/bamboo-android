60394efd1c20027b2b2badbb82b6b24bf2e9dd84
ckrudals <2004ckck@naver.com>
2022-03-17 16:16:04 +0900

    🐛 :: PnedingFinsih navigation action

--- presentation/src/main/java/com/study/presentation/view/admin/dialog/PendingDialog.kt
+++ presentation/src/main/java/com/study/presentation/view/admin/dialog/PendingDialog.kt
@@ -3,6 +3,7 @@ package com.study.presentation.view.admin.dialog
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -12,9 +13,11 @@ import com.study.domain.model.admin.request.SetStatusEntity
 import com.study.presentation.R
 import com.study.presentation.adapter.STATUS
 import com.study.presentation.databinding.PendingDialogBinding
+import com.study.presentation.utils.getDialogNavResult
 import com.study.presentation.utils.setNavResult
 import com.study.presentation.view.admin.AdminViewModel
 import com.study.presentation.view.admin.AuthViewModel
+import com.study.presentation.view.user.viewmodel.AlgorithmViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -24,16 +27,15 @@ class PendingDialog : BaseDialogFragment<PendingDialogBinding>(R.layout.pending_
     private val args by navArgs<PendingDialogArgs>()
     private val viewModel: AdminViewModel by viewModels()
     private val authViewModel: AuthViewModel by viewModels()
+    private val algorithmViewModel: AlgorithmViewModel by activityViewModels()
 
 
     override fun PendingDialogBinding.onViewCreated() {
+
         with(viewModel) {
             isSuccess.observe(viewLifecycleOwner) {
                 if (it.contains(STATUS.ACCEPTED.toString())) {
                     setNavResult(data = STATUS.ACCEPTED)
-                    findNavController().popBackStack()
-                } else {
-                    setNavResult(data = STATUS.REJECTED)
                     findNavController().popBackStack()
                 }
 
@@ -50,6 +52,14 @@ class PendingDialog : BaseDialogFragment<PendingDialogBinding>(R.layout.pending_
                 Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
             }
         }
+        val id = findNavController().currentDestination?.id
+        id?.let {
+            getDialogNavResult<STATUS>(navId = it) { result ->
+                setNavResult(data = STATUS.REJECTED)
+                findNavController().popBackStack()
+
+            }
+        }
     }
 
     fun acceptPost() {
@@ -58,25 +68,20 @@ class PendingDialog : BaseDialogFragment<PendingDialogBinding>(R.layout.pending_
             viewModel.patchPost(
                 authViewModel.getToken(),
                 args.result.idx,
-                SetStatusEntity(STATUS.ACCEPTED.toString(),binding.reasonText.text.toString())
+                SetStatusEntity(STATUS.ACCEPTED.toString(), "")
             )
         }
     }
-
 
     fun rejectPost() {
 
-        lifecycleScope.launch {
-            viewModel.patchPost(
-                authViewModel.getToken(),
-                args.result.idx,
-                SetStatusEntity(STATUS.REJECTED.toString(),binding.reasonText.text.toString())
-            )
-        }
+        findNavController().navigate(R.id.action_pendingDialog_to_pendingFinishDialog)
+        algorithmViewModel.getIdx(args.result.idx)
 
     }
 
+
     override fun PendingDialogBinding.onCreateView() {
-        binding.dialog=this@PendingDialog
+        binding.dialog = this@PendingDialog
     }
 }
